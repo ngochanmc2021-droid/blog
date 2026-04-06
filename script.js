cd53344ec40a9b2eea1429746cfcadad7dd607ef
ngochanmc2021-droid <ngochanmc2021@gmail.com>
2026-04-06 20:01:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Danh sách 78 lá bài với ID tương ứng kho ảnh trực tuyến
 const tarotDeck = [
-    { id: 0, name: 'The Fool', vi: 'Chàng Khờ' },
-    { id: 1, name: 'The Magician', vi: 'Nhà Ảo Thuật' },
-    { id: 2, name: 'The High Priestess', vi: 'Nữ Tư Tế' },
-    { id: 3, name: 'The Empress', vi: 'Nữ Hoàng' },
-    { id: 4, name: 'The Emperor', vi: 'Hoàng Đế' },
-    { id: 5, name: 'The Hierophant', vi: 'Thầy Tư Tế' },
-    { id: 6, name: 'The Lovers', vi: 'Tình Nhân' },
-    { id: 7, name: 'The Chariot', vi: 'Chiến Xe' },
-    { id: 8, name: 'Strength', vi: 'Sức Mạnh' },
-    { id: 9, name: 'The Hermit', vi: 'Ẩn Sĩ' },
-    { id: 10, name: 'Wheel of Fortune', vi: 'Vòng Quay May Mắn' },
-    { id: 11, name: 'Justice', vi: 'Công Lý' },
-    { id: 12, name: 'The Hanged Man', vi: 'Kẻ Treo' },
-    { id: 13, name: 'Death', vi: 'Cái Chết' },
-    { id: 14, name: 'Temperance', vi: 'Tiết Độ' },
-    { id: 15, name: 'The Devil', vi: 'Ác Quỷ' },
-    { id: 16, name: 'The Tower', vi: 'Tòa Tháp' },
-    { id: 17, name: 'The Star', vi: 'Ngôi Sao' },
-    { id: 18, name: 'The Moon', vi: 'Mặt Trăng' },
-    { id: 19, name: 'The Sun', vi: 'Mặt Trời' },
-    { id: 20, name: 'Judgement', vi: 'Phán Xét' },
-    { id: 21, name: 'The World', vi: 'Thế Giới' }
+    const tarotDeck = [
+    { id: 0, name: 'The Fool', vi: 'Chàng Khờ', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m00.jpg' },
+    { id: 1, name: 'The Magician', vi: 'Nhà Ảo Thuật', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m01.jpg' },
+    { id: 2, name: 'The High Priestess', vi: 'Nữ Tư Tế', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m02.jpg' },
+    { id: 3, name: 'The Empress', vi: 'Nữ Hoàng', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m03.jpg' },
+    { id: 4, name: 'The Emperor', vi: 'Hoàng Đế', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m04.jpg' },
+    { id: 5, name: 'The Hierophant', vi: 'Thầy Tư Tế', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m05.jpg' },
+    { id: 6, name: 'The Lovers', vi: 'Tình Nhân', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m06.jpg' },
+    { id: 7, name: 'The Chariot', vi: 'Chiến Xe', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m07.jpg' },
+    { id: 8, name: 'Strength', vi: 'Sức Mạnh', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m08.jpg' },
+    { id: 9, name: 'The Hermit', vi: 'Ẩn Sĩ', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m09.jpg' },
+    { id: 10, name: 'Wheel of Fortune', vi: 'Vòng Quay May Mắn', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m10.jpg' },
+    { id: 11, name: 'Justice', vi: 'Công Lý', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m11.jpg' },
+    { id: 12, name: 'The Hanged Man', vi: 'Kẻ Treo', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m12.jpg' },
+    { id: 13, name: 'Death', vi: 'Cái Chết', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m13.jpg' },
+    { id: 14, name: 'Temperance', vi: 'Tiết Độ', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m14.jpg' },
+    { id: 15, name: 'The Devil', vi: 'Ác Quỷ', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m15.jpg' },
+    { id: 16, name: 'The Tower', vi: 'Tòa Tháp', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m16.jpg' },
+    { id: 17, name: 'The Star', vi: 'Ngôi Sao', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m17.jpg' },
+    { id: 18, name: 'The Moon', vi: 'Mặt Trăng', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m18.jpg' },
+    { id: 19, name: 'The Sun', vi: 'Mặt Trời', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m19.jpg' },
+    { id: 20, name: 'Judgement', vi: 'Phán Xét', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m20.jpg' },
+    { id: 21, name: 'The World', vi: 'Thế Giới', image: 'https://raw.githubusercontent.com/ekelen/tarot-api/master/static/cards/m21.jpg' }
+];
     // ... bạn có thể thêm các lá Ẩn phụ nếu muốn, logic vẫn tương tự
 ];
 
